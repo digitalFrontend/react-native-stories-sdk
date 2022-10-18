1fc573a0d03cd3300decc09918a27597bfbf0377
Artem Avdzhiyan <artem.avdzhiyan@tele2.ru>
2022-10-18 12:06:01 +0300

    native view in RN from library for Android

---
+++ android/build/generated/source/buildConfig/debug/ru/nasvyazi/stories/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package ru.nasvyazi.stories;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "ru.nasvyazi.stories";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
---
+++ android/src/main/java/ru/nasvyazi/stories/MyFragment.java
@@ -0,0 +1,55 @@
+package ru.nasvyazi.stories;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+
+import androidx.fragment.app.Fragment;
+
+
+import ru.nasvyazi.stories.CustomView;
+
+public class MyFragment extends Fragment {
+    CustomView customView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
+        super.onCreateView(inflater, parent, savedInstanceState);
+//        LayoutInflater inflater1 = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.stories_layout, null);
+
+        FrameLayout item = (FrameLayout ) view.findViewById(R.id.frameLayout1);
+//        customView = new CustomView(this.getContext());
+        return view; // this CustomView could be any view that you want to render
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        // do any logic that should happen in an `onCreate` method, e.g:
+        // customView.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        // do any logic that should happen in an `onPause` method
+        // e.g.: customView.onPause();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        // do any logic that should happen in an `onResume` method
+        // e.g.: customView.onResume();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        // do any logic that should happen in an `onDestroy` method
+        // e.g.: customView.onDestroy();
+    }
+}
